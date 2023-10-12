c46c5e4ae5b5f07eef080031fa018c0b3432e107
Brayan <Brayan@SOPORTE-TIC>
2023-10-12 14:03:24 -0500

    Agregar archivos de proyecto.

---
+++ Poo2/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Poo2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grbTipoPersona = new System.Windows.Forms.GroupBox();
+            this.radEstudiante = new System.Windows.Forms.RadioButton();
+            this.radProfesor = new System.Windows.Forms.RadioButton();
+            this.grbDatos = new System.Windows.Forms.GroupBox();
+            this.lblCalificacion = new System.Windows.Forms.Label();
+            this.txtCalificacion = new System.Windows.Forms.TextBox();
+            this.lblSueldo = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtSueldo = new System.Windows.Forms.TextBox();
+            this.btnCapturarDatos = new System.Windows.Forms.Button();
+            this.btnMostrarDatos = new System.Windows.Forms.Button();
+            this.dgvListaDatos = new System.Windows.Forms.DataGridView();
+            this.grbTipoPersona.SuspendLayout();
+            this.grbDatos.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListaDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grbTipoPersona
+            // 
+            this.grbTipoPersona.Controls.Add(this.radEstudiante);
+            this.grbTipoPersona.Controls.Add(this.radProfesor);
+            this.grbTipoPersona.Location = new System.Drawing.Point(21, 22);
+            this.grbTipoPersona.Name = "grbTipoPersona";
+            this.grbTipoPersona.Size = new System.Drawing.Size(213, 130);
+            this.grbTipoPersona.TabIndex = 0;
+            this.grbTipoPersona.TabStop = false;
+            this.grbTipoPersona.Text = "Tipo de Persona";
+            // 
+            // radEstudiante
+            // 
+            this.radEstudiante.AutoSize = true;
+            this.radEstudiante.Location = new System.Drawing.Point(21, 83);
+            this.radEstudiante.Name = "radEstudiante";
+            this.radEstudiante.Size = new System.Drawing.Size(75, 17);
+            this.radEstudiante.TabIndex = 2;
+            this.radEstudiante.TabStop = true;
+            this.radEstudiante.Text = "Estudiante";
+            this.radEstudiante.UseVisualStyleBackColor = true;
+            // 
+            // radProfesor
+            // 
+            this.radProfesor.AutoSize = true;
+            this.radProfesor.Location = new System.Drawing.Point(21, 41);
+            this.radProfesor.Name = "radProfesor";
+            this.radProfesor.Size = new System.Drawing.Size(64, 17);
+            this.radProfesor.TabIndex = 1;
+            this.radProfesor.TabStop = true;
+            this.radProfesor.Text = "Profesor";
+            this.radProfesor.UseVisualStyleBackColor = true;
+            // 
+            // grbDatos
+            // 
+            this.grbDatos.Controls.Add(this.lblCalificacion);
+            this.grbDatos.Controls.Add(this.txtCalificacion);
+            this.grbDatos.Controls.Add(this.lblSueldo);
+            this.grbDatos.Controls.Add(this.txtNombre);
+            this.grbDatos.Controls.Add(this.lblNombre);
+            this.grbDatos.Controls.Add(this.txtSueldo);
+            this.grbDatos.Location = new System.Drawing.Point(21, 226);
+            this.grbDatos.Name = "grbDatos";
+            this.grbDatos.Size = new System.Drawing.Size(213, 143);
+            this.grbDatos.TabIndex = 1;
+            this.grbDatos.TabStop = false;
+            this.grbDatos.Text = "Datos de la Persona";
+            // 
+            // lblCalificacion
+            // 
+            this.lblCalificacion.AutoSize = true;
+            this.lblCalificacion.Location = new System.Drawing.Point(18, 92);
+            this.lblCalificacion.Name = "lblCalificacion";
+            this.lblCalificacion.Size = new System.Drawing.Size(61, 13);
+            this.lblCalificacion.TabIndex = 4;
+            this.lblCalificacion.Text = "Calificacion";
+            // 
+            // txtCalificacion
+            // 
+            this.txtCalificacion.Location = new System.Drawing.Point(118, 85);
+            this.txtCalificacion.Name = "txtCalificacion";
+            this.txtCalificacion.Size = new System.Drawing.Size(89, 20);
+            this.txtCalificacion.TabIndex = 4;
+            // 
+            // lblSueldo
+            // 
+            this.lblSueldo.AutoSize = true;
+            this.lblSueldo.Location = new System.Drawing.Point(18, 66);
+            this.lblSueldo.Name = "lblSueldo";
+            this.lblSueldo.Size = new System.Drawing.Size(40, 13);
+            this.lblSueldo.TabIndex = 3;
+            this.lblSueldo.Text = "Sueldo";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(101, 33);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(106, 20);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(18, 33);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.TabIndex = 2;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // txtSueldo
+            // 
+            this.txtSueldo.Location = new System.Drawing.Point(101, 59);
+            this.txtSueldo.Name = "txtSueldo";
+            this.txtSueldo.Size = new System.Drawing.Size(106, 20);
+            this.txtSueldo.TabIndex = 3;
+            // 
+            // btnCapturarDatos
+            // 
+            this.btnCapturarDatos.Location = new System.Drawing.Point(373, 37);
+            this.btnCapturarDatos.Name = "btnCapturarDatos";
+            this.btnCapturarDatos.Size = new System.Drawing.Size(115, 85);
+            this.btnCapturarDatos.TabIndex = 2;
+            this.btnCapturarDatos.Text = "Capturar Datos";
+            this.btnCapturarDatos.UseVisualStyleBackColor = true;
+            // 
+            // btnMostrarDatos
+            // 
+            this.btnMostrarDatos.Location = new System.Drawing.Point(373, 128);
+            this.btnMostrarDatos.Name = "btnMostrarDatos";
+            this.btnMostrarDatos.Size = new System.Drawing.Size(115, 88);
+            this.btnMostrarDatos.TabIndex = 3;
+            this.btnMostrarDatos.Text = "Mostrar Datos";
+            this.btnMostrarDatos.UseVisualStyleBackColor = true;
+            // 
+            // dgvListaDatos
+            // 
+            this.dgvListaDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListaDatos.Location = new System.Drawing.Point(399, 260);
+            this.dgvListaDatos.Name = "dgvListaDatos";
+            this.dgvListaDatos.Size = new System.Drawing.Size(446, 272);
+            this.dgvListaDatos.TabIndex = 4;
+            this.dgvListaDatos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(910, 540);
+            this.Controls.Add(this.dgvListaDatos);
+            this.Controls.Add(this.btnMostrarDatos);
+            this.Controls.Add(this.btnCapturarDatos);
+            this.Controls.Add(this.grbDatos);
+            this.Controls.Add(this.grbTipoPersona);
+            this.Name = "Form1";
+            this.Text = "PERSONAS DE UNA ESCUELA";
+            this.grbTipoPersona.ResumeLayout(false);
+            this.grbTipoPersona.PerformLayout();
+            this.grbDatos.ResumeLayout(false);
+            this.grbDatos.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListaDatos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grbTipoPersona;
+        private System.Windows.Forms.RadioButton radProfesor;
+        private System.Windows.Forms.RadioButton radEstudiante;
+        private System.Windows.Forms.GroupBox grbDatos;
+        private System.Windows.Forms.TextBox txtCalificacion;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtSueldo;
+        private System.Windows.Forms.Label lblCalificacion;
+        private System.Windows.Forms.Label lblSueldo;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Button btnCapturarDatos;
+        private System.Windows.Forms.Button btnMostrarDatos;
+        private System.Windows.Forms.DataGridView dgvListaDatos;
+    }
+}
+
